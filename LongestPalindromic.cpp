c9246400fb0e1704fb37d9ddd32009c3a6452648
Hyan Lee <hyan23lee@hotmail.com>
2019-03-05 00:41:31 +0800

    Formatted documents

--- LongestPalindromic.cpp
+++ LongestPalindromic.cpp
@@ -7,68 +7,68 @@
 
 using namespace std;
 
-class Solution {
-public:
-	string longestPalindrome(string s) {
-		int maxLength = 0, start = 0, end = 0;
-		for (int i = 0; i < s.length(); i++)
-		{
-			// match aba type
-			int m = i, n = i;
-			int length = 1;
-			while (true)
-			{
-				m--, n++;
-				if (m < 0 || n >= s.length())
-				{
-					break;
-				}
-				if (s[m] != s[n])
-				{
-					break;
-				}
-			}
-			length = n - m - 1;
-			if (length > maxLength)
-			{
-				maxLength = length;
-				start = m + 1;
-				end = n - 1;
-			}
-			// match abba type
-			m = i, n = i + 1;
-			length = 1;
-			while (true)
-			{
-				if (s[m] != s[n])
-				{
-					break;
-				}
-				m--, n++;
-				if (m < 0 || n >= s.length())
-				{
-					break;
-				}
-			}
-			length = n - m - 1;
-			if (length > maxLength)
-			{
-				maxLength = length;
-				start = m + 1;
-				end = n + 1;
-			}
-		}
-		return s.substr(start, maxLength);
-	}
+class Solution
+{
+  public:
+    string longestPalindrome(string s)
+    {
+        int maxLength = 0, start = 0;
+        for (int i = 0; i < s.length(); i++)
+        {
+            // match aba type
+            int m = i, n = i;
+            int length = 1;
+            while (true)
+            {
+                m--, n++;
+                if (m < 0 || n >= s.length())
+                {
+                    break;
+                }
+                if (s[m] != s[n])
+                {
+                    break;
+                }
+            }
+            length = n - m - 1;
+            if (length > maxLength)
+            {
+                maxLength = length;
+                start = m + 1;
+            }
+            // match abba type
+            m = i, n = i + 1;
+            length = 1;
+            while (true)
+            {
+                if (s[m] != s[n])
+                {
+                    break;
+                }
+                m--, n++;
+                if (m < 0 || n >= s.length())
+                {
+                    break;
+                }
+            }
+            length = n - m - 1;
+            if (length > maxLength)
+            {
+                maxLength = length;
+                start = m + 1;
+            }
+        }
+        return s.substr(start, maxLength);
+    }
 };
 
 int main(int argc, char *argv[])
 {
     (void)argc;
     (void)argv;
-    
-    cout << longestPalindrome("babad") << endl;
-    cout << longestPalindrome("cbbd") << endl;
-    
+
+    cout << Solution().longestPalindrome("babad") << endl;
+    cout << Solution().longestPalindrome("cbbd") << endl;
+
     return 0;
 }
--- StringToInteger.cpp
+++ StringToInteger.cpp
@@ -4,70 +4,73 @@
 // https://leetcode.com/problems/string-to-integer-atoi/
 
 #include <iostream>
+#include <climits>
 
 using namespace std;
 
-class Solution {
-public:
-	int myAtoi(string str) {
-		char sign = '+';
-		bool started = false;
-		int result = 0;
+class Solution
+{
+  public:
+    int myAtoi(string str)
+    {
+        char sign = '+';
+        bool started = false;
+        int result = 0;
 
-		for (int i = 0; i < str.length(); i++)
-		{
-			if (!started)
-			{
-				if (str[i] != ' ')
-				{
-					started = true;
-					if (str[i] == '+' || str[i] == '-')
-					{
-						sign = str[i];
-						continue;
-					}
-				}
-			}
-			if (started)
-			{
-				if (!isdigit(str[i]))
-				{
-					break;
-				}
-				int digit = str[i] - '0';
-				if (sign == '-')
-				{
-					digit = -digit;
-				}
-				// INT_MAX: 2147483647
-				if (result > INT_MAX / 10 || result > INT_MAX / 10 - 1 && digit > 7)
-				{
-					result = INT_MAX;
-					break;
-				}
-				// INT_MIN: -2147483648
-				if (result < INT_MIN / 10 || result < INT_MIN / 10 + 1 && digit < -8)
-				{
-					result = INT_MIN;
-					break;
-				}
-				result = result * 10 + digit;
-			}
-		}
-		return result;
-	}
+        for (int i = 0; i < str.length(); i++)
+        {
+            if (!started)
+            {
+                if (str[i] != ' ')
+                {
+                    started = true;
+                    if (str[i] == '+' || str[i] == '-')
+                    {
+                        sign = str[i];
+                        continue;
+                    }
+                }
+            }
+            if (started)
+            {
+                if (!isdigit(str[i]))
+                {
+                    break;
+                }
+                int digit = str[i] - '0';
+                if (sign == '-')
+                {
+                    digit = -digit;
+                }
+                // INT_MAX: 2147483647
+                if (result > INT_MAX / 10 || (result > INT_MAX / 10 - 1 && digit > 7))
+                {
+                    result = INT_MAX;
+                    break;
+                }
+                // INT_MIN: -2147483648
+                if (result < INT_MIN / 10 || (result < INT_MIN / 10 + 1 && digit < -8))
+                {
+                    result = INT_MIN;
+                    break;
+                }
+                result = result * 10 + digit;
+            }
+        }
+        return result;
+    }
 };
 
 int main(int argc, char *argv[])
 {
     (void)argc;
     (void)argv;
-    
-    cout << myAtoi("42") << endl;
-    cout << myAtoi("   -42") << endl;
-    cout << myAtoi("4193 with words") << endl;
-    cout << myAtoi("words and 987") << endl;
-    cout << myAtoi("-91283472332") << endl;
+
+    cout << Solution().myAtoi("42") << endl;
+    cout << Solution().myAtoi("   -42") << endl;
+    cout << Solution().myAtoi("4193 with words") << endl;
+    cout << Solution().myAtoi("words and 987") << endl;
+    cout << Solution().myAtoi("-91283472332") << endl;
 
     return 0;
 }
