4e407208782d6d03e37833beb4f3c25cce82d093
Hyan Lee <hyan23lee@hotmail.com>
2019-03-10 18:37:16 +0800

    Added LongestCommonPrefix.go

---
+++ LongestCommonPrefix.go
@@ -0,0 +1,14 @@
+// LongestCommonPrefix.cpp
+// Author: hyan23
+// 2019.03.10
+// https://leetcode.com/problems/longest-common-prefix/
+
+package main
+
+import(
+	"fmt"
+)
+
+func main(){
+	
+}
---
+++ template.go
@@ -0,0 +1,14 @@
+// .cpp
+// Author: hyan23
+// <yyyy.mm.dd>
+// <problem url>
+
+package main
+
+import(
+	"fmt"
+)
+
+func main(){
+	
+}
