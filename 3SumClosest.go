34b136b6cd8778b1bd4301fb38884d99d0d16829
Hyan Lee <hyan23lee@hotmail.com>
2019-03-11 18:31:16 +0800

    Added 3SumClosest.go

---
+++ 3SumClosest.go
@@ -0,0 +1,18 @@
+// 3SumClosest.cpp
+// Author: hyan23
+// 2019.03.11
+// https://leetcode.com/problems/3sum-closest/
+
+package main
+
+import(
+	"fmt"
+)
+
+func threeSumClosest(nums []int, target int) int {
+	
+}
+
+func main(){
+	
+}
