857155073288e20280c5ec7ffeb43c07b2b0cadf
Hyan Lee <hyan23lee@hotmail.com>
2019-03-18 15:50:12 +0800

    Added 3SumClosest.go

--- LongestCommonPrefix.go
+++ LongestCommonPrefix.go
@@ -5,10 +5,41 @@
 
 package main
 
-import(
+import (
 	"fmt"
 )
 
-func main(){
-	
+func longestCommonPrefix(strs []string) string {
+	prefix := ""
+	pos := 0
+	for len(strs) > 0 {
+		match := true
+		var letter byte = '#'
+		for _, str := range strs {
+			if pos == len(str) {
+				match = false
+				break
+			}
+			if letter == '#' {
+				letter = str[pos]
+			}
+			if str[pos] != letter {
+				match = false
+				break
+			}
+		}
+		if match {
+			prefix = prefix + string(letter)
+		} else {
+			break
+		}
+		pos++
+	}
+	return prefix
+}
+
+func main() {
+	fmt.Println(longestCommonPrefix([]string{"flower", "flow", "flight"}))
+	fmt.Println(longestCommonPrefix([]string{"dog", "racecar", "car"}))
+	fmt.Println(longestCommonPrefix([]string{}))
 }
