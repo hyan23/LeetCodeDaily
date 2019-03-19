1b114bbd7dbdf936373e038d0b51ccde22ca655e
Hyan Lee <hyan23lee@hotmail.com>
2019-03-19 12:30:33 +0800

    Added RemoveNthNode.go

---
+++ ValidParentheses.go
@@ -0,0 +1,20 @@
+// ValidParentheses.go
+// Author: hyan23
+// 2019.03.19
+// https://leetcode.com/problems/valid-parentheses/
+
+package main
+
+// TODO
+
+func isValid(s string) bool {
+
+}
+
+func main() {
+	fmt.Println(isValid("()"))
+	fmt.Println(isValid("()[]{}"))
+	fmt.Println(isValid("(]")
+	fmt.Println(isValid("([)]")
+	fmt.Println(isValid("{[]}")
+}
