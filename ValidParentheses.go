2fe1641ffc78e0055b0816b679b6f33f6d0d7ae6
Hyan Lee <hyan23lee@hotmail.com>
2019-03-20 14:37:54 +0800

    Added ValidParentheses.go

--- ValidParentheses.go
+++ ValidParentheses.go
@@ -5,16 +5,41 @@
 
 package main
 
-// TODO
+import "fmt"
 
 func isValid(s string) bool {
-
+	partner := map[rune]rune{
+		')': '(',
+		']': '[',
+		'}': '{',
+	}
+	sta := make([]rune, 0)
+	for _, ch := range s {
+		if ch == '(' || ch == '[' || ch == '{' {
+			sta = append(sta, ch)
+		} else if ch == ')' || ch == ']' || ch == '}' {
+			if len(sta) > 0 {
+				if sta[len(sta)-1] != partner[ch] {
+					return false
+				}
+				sta = sta[0 : len(sta)-1]
+			} else {
+				return false
+			}
+		}
+	}
+	if len(sta) > 0 {
+		return false
+	} else {
+		return true
+	}
 }
 
 func main() {
 	fmt.Println(isValid("()"))
 	fmt.Println(isValid("()[]{}"))
-	fmt.Println(isValid("(]")
-	fmt.Println(isValid("([)]")
-	fmt.Println(isValid("{[]}")
+	fmt.Println(isValid("(]"))
+	fmt.Println(isValid("([)]"))
+	fmt.Println(isValid("{[]}"))
+	fmt.Println(isValid(""))
 }
