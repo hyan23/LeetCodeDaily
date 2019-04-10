954294a35f0199d7d3bd376d5ebbbba74cb6dbb5
Hyan Lee <hyan23lee@hotmail.com>
2019-04-10 17:45:00 +0800

    Added LongestValidParentheses.go

---
+++ archive/20190410/cpp/template.cpp
@@ -0,0 +1,18 @@
+// .cpp
+// Author: hyan23
+// <yyyy.mm.dd>
+// <problem url>
+
+#include <iostream>
+
+using namespace std;
+
+// Paste the solution class here
+
+int main(int argc, char *argv[])
+{
+    (void)argc;
+    (void)argv;
+    
+    return 0;
+}
