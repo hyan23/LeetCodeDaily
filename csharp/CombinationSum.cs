769ba929e9489690290f5f4f15703cc1993998f8
Hyan Lee <hyan23lee@hotmail.com>
2019-10-20 19:17:22 +0800

    Added FirstMissingPositive.cs

--- csharp/CombinationSum.cs
+++ csharp/CombinationSum.cs
@@ -3,8 +3,38 @@
 // 2019.10.20
 // https://leetcode.com/problems/combination-sum/
 
-// Runtime: 284 ms (23.5% faster)
-// Best solution: 232 ms
+/*
+Given a set of candidate numbers (candidates) (without duplicates) and a target number (target), find all unique combinations in candidates where the candidate numbers sums to target.
+
+The same repeated number may be chosen from candidates unlimited number of times.
+
+Note:
+
+All numbers (including target) will be positive integers.
+The solution set must not contain duplicate combinations.
+Example 1:
+
+Input: candidates = [2,3,6,7], target = 7,
+A solution set is:
+[
+  [7],
+  [2,2,3]
+]
+Example 2:
+
+Input: candidates = [2,3,5], target = 8,
+A solution set is:
+[
+  [2,2,2,2],
+  [2,3,3],
+  [3,5]
+]
+ */
+
+/*
+Runtime: 296 ms, faster than 12.98% of C# online submissions for Combination Sum.
+Memory Usage: 31.4 MB, less than 25.00% of C# online submissions for Combination Sum.
+ */
 
 using System;
 using System.Collections.Generic;
@@ -46,11 +76,11 @@ namespace csharp
         public IList<IList<int>> CombinationSum(int[] candidates, int target)
         {
             List<IList<int>> results = new List<IList<int>>();
-            Helper(candidates, target, 0, new List<int>(), results);
+            Helper(candidates, 0, target, new List<int>(), results);
             return results;
         }
 
-        public void Helper(int[] candidates, int target, int index, List<int> combo, List<IList<int>> results)
+        public void Helper(int[] candidates, int index, int target, List<int> combo, List<IList<int>> results)
         {
             if (index > candidates.Length - 1)
             {
@@ -64,7 +94,7 @@ namespace csharp
                 {
                     List<int> combo2 = new List<int>(combo);
                     combo2.Add(candidates[i]);
-                    Helper(candidates, target - candidates[i], i, combo2, results);
+                    Helper(candidates, i, target - candidates[i], combo2, results);
                 }
             }
             else if (target == 0)
