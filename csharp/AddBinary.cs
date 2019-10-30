640e78fca669507e62544886a007d3c6612b42ee
Hyan Lee <hyan23lee@hotmail.com>
2019-10-30 20:11:55 +0800

    Added PlusOne.cs

---
+++ csharp/AddBinary.cs
@@ -0,0 +1,32 @@
+// AddBinary.cs
+// Author: hyan23
+// 2019.10.30
+// https://leetcode.com/problems/add-binary/
+
+/*
+Given two binary strings, return their sum (also a binary string).
+The input strings are both non-empty and contains only characters 1 or 0.
+Example 1:
+Input: a = "11", b = "1"
+Output: "100"
+Example 2:
+Input: a = "1010", b = "1011"
+Output: "10101"
+ */
+
+// <Analysis>
+
+namespace csharp
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            new Program().Test();
+        }
+
+        public void Test()
+        {
+        }
+    }
+}
