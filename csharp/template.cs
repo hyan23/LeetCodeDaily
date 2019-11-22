34f9a801a10a3096cda487ea54ab243f88e52d92
Hyan Lee <hyan23lee@hotmail.com>
2019-11-22 13:54:34 +0800

    Added RemoveDuplicatesFromSortedListII.cs

--- csharp/template.cs
+++ csharp/template.cs
@@ -6,7 +6,41 @@
 // <Analysis>
 
 namespace csharp
-{
+{    
+    public class ListNode
+    {
+        public int val;
+        public ListNode next;
+        public ListNode(int x) { val = x; }
+    }
+
+    public static class MyExtensions
+    {
+        public static ListNode ToLinkedList(this int[] arr)
+        {
+            Trace.Assert(arr.Length > 0);
+            ListNode head = new ListNode(arr[0]);
+            ListNode cur = head;
+            for (int i = 1; i < arr.Length; i++)
+            {
+                cur.next = new ListNode(arr[i]);
+                cur = cur.next;
+            }
+            return head;
+        }
+
+        public static int[] ToArray(this ListNode head)
+        {
+            List<int> list = new List<int>();
+            while (head != null)
+            {
+                list.Add(head.val);
+                head = head.next;
+            }
+            return list.ToArray();
+        }
+    }
+
     public class Program
     {
         public static void Main()
